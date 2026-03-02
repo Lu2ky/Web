e84d1e6c7479077f486f8c95149f0475fad6f9e9
Lu2ky <jaaa736504@gmail.com>
2026-03-01 22:09:57 -0500

    another fix

--- src/services/ToDoListTagFetcher.jsx
+++ src/services/ToDoListTagFetcher.jsx
@@ -28,7 +28,7 @@ function ToDoListTagFetcher({ onDataLoaded }) {
     useEffect(() => {
         const fetchTags = async () => {
             try {
-                const response = await fetch("http://209.25.140.20:3380/api/get-tags");
+                const response = await fetch("http://209.25.140.25:9242/api/get-tags");
                 const json = await response.json();
                 const rawData = Array.isArray(json?.data)
                     ? json.data
