6b8283238fe1c3a6130f8aeb7fff970c7bba290d
G-Gdeveloper <dannaga0809@gmail.com>
2026-03-18 06:05:39 -0500

    Funcionalidad Cerrar Sesión

--- src/components/Account/DropdownAcount.jsx
+++ src/components/Account/DropdownAcount.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Modal from "./Modal";
 import UserProfile from "./UserProfile";
 import UserPreferences from "./UserPreferences";
@@ -7,13 +8,15 @@ import "./DropdownAcount.css";
 const OPTIONS = [
     { id: "acount", label: "Mi Perfil" },
     { id: "prefer", label: "Preferencias" },
-    { id: "close", label: "Cerrar Sesión"},
+    { id: "close", label: "Cerrar Sesión" },
 ];
 
 export default function DropdownAcount({ userId }) {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [activeModal, setActiveModal] = useState(null);
     const dropdownRef = useRef(null);
+    const navigate = useNavigate();
+
 
     useEffect(() => {
         function handleClickOutside(event) {
@@ -30,6 +33,14 @@ export default function DropdownAcount({ userId }) {
     const handleOptionClick = (optionId) => {
         setActiveModal(optionId);
         setIsDropdownOpen(false);
+    };
+
+    const handleLogout = () => {
+        // Aquí limpias la sesión (ejemplo: borrar token)
+        localStorage.removeItem("token");
+
+        // Rediriges al login
+        navigate("/");
     };
 
     const closeModal = () => setActiveModal(null);
@@ -89,7 +100,12 @@ export default function DropdownAcount({ userId }) {
                 title="Cerrar Sesión"
             >
                 <p>¿Estás seguro de que deseas cerrar sesión?</p>
-                <button className="modal-confirm-btn">Sí, cerrar sesión</button>
+                <button
+                    className="modal-confirm-btn"
+                    onClick={handleLogout}
+                >
+                    Sí, cerrar sesión
+                </button>
             </Modal>
         </div>
     );
